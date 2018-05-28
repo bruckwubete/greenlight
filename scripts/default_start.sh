6d09b8ddfd131b2c220d24774b6ca68cb420c700
bruckwubete <bruckwendwessenwubet@cmail.carleton.ca>
2018-05-28 11:57:00 -0400

    update start script to use bundle to run thin server

--- scripts/default_start.sh
+++ scripts/default_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bundle exec  rake db:exists && bundle exec rake db:migrate || bundle exec rake db:setup
-thin start --ssl --ssl-verify --ssl-key-file ssl/server.key --ssl-cert-file ssl/greenlight-server.crt -p 3000
+bundle exec rake db:exists && bundle exec rake db:migrate || bundle exec rake db:setup
+bundle exec thin start --ssl --ssl-verify --ssl-key-file ssl/server.key --ssl-cert-file ssl/greenlight-server.crt -p 3000
